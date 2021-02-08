bf17e887d39729ce2c9ef0debeda49f2eea03464
Mohammed Thaabit Jacobs <mohammedthaabitjacobs@gmail.com>
2021-02-08 12:21:43 +0200

    first commit

---
+++ src/main/java/com/global/connect/GlobalConnectApplication.java
@@ -0,0 +1,13 @@
+package com.global.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalConnectApplication.class, args);
+	}
+
+}
